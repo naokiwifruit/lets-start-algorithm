d3ffd836d62e06d2453d4ea202799e6fbdcf6edc
naokiwifruit <raye4483@gmail.com>
2020-04-24 20:55:34 +0900

    add ch3/compare_two_data.py

---
+++ # lets-start-algorithm.md
@@ -0,0 +1,2 @@
+# lets-start-algorithm
+Python codes implementing algorithms described in Ito's book "アルゴリズムを、はじめよう"
---
+++ ch3/compare_two_data.py
@@ -0,0 +1,13 @@
+def compare_two_data(a, b):
+    """
+    :type a:int
+    :type b:int
+    :rtype: int
+    """
+    if a > b:
+        return a
+    else:
+        if a == b:
+            print('aとbは等しい')
+        else:
+            print(b)
